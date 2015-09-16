42139486ecf5bfe2c590319ddb5f2497a656565a
ryssen <ryssen@users.noreply.github.com>
2015-09-16 13:17:24 +0200

    test3

--- app/src/main/java/com/example/eandreje/androidapp/MainActivity.java
+++ app/src/main/java/com/example/eandreje/androidapp/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
-    //hej då
+    //hej då igen
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
