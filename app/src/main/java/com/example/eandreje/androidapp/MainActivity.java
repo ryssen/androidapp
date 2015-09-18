c5bf4f74d4c0158c1e5789bbda25649b7f7e0bb0
ryssen <ryssen@users.noreply.github.com>
2015-09-18 15:59:39 +0200

    lagt till actionbar, och klasser

--- app/src/main/java/com/example/eandreje/androidapp/MainActivity.java
+++ app/src/main/java/com/example/eandreje/androidapp/MainActivity.java
@@ -1,18 +1,10 @@
 package com.example.eandreje.androidapp;
 
-import android.content.DialogInterface;
-import android.os.ParcelFileDescriptor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListAdapter;
 import android.widget.Toast;
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.add_activity_icon:
+
+                Toast.makeText(MainActivity.this, "Ny aktivitet tillagd", Toast.LENGTH_SHORT).show();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
