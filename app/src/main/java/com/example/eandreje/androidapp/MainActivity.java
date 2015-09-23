1be38b31d96acfb77c598d2db3f3f579e90cdd26
ryssen <ryssen@users.noreply.github.com>
2015-09-23 11:39:22 +0200

    börjat med fragment, skapat listview listener.

--- app/src/main/java/com/example/eandreje/androidapp/MainActivity.java
+++ app/src/main/java/com/example/eandreje/androidapp/MainActivity.java
@@ -1,18 +1,41 @@
 package com.example.eandreje.androidapp;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView activityList;
+    ArrayAdapter<String> activityAdapter;
+    String[] newActivity = {"Fotboll", "Konferens", "Möte"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        activityAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, newActivity);
+        activityList = (ListView)findViewById(R.id.listView);
+        activityList.setAdapter(activityAdapter);
+
+        //Item listener
+        AdapterView.OnItemClickListener itemClick = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                view.setBackgroundResource(R.color.main_text_color);
+            }
+        };
+        activityList.setOnItemClickListener(itemClick);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
