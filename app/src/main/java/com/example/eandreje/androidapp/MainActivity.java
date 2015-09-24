f720e3746c076b4b84dc95e7c3f597b6c982b2b8
johaerik <johanneseriksson89@hotmail.com>
2015-09-24 11:58:35 +0200

    Lägg till, ändra, ta bort fungerar, update

--- app/src/main/java/com/example/eandreje/androidapp/MainActivity.java
+++ app/src/main/java/com/example/eandreje/androidapp/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.eandreje.androidapp;
 
 import android.app.AlertDialog;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,10 +12,9 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
+
 import android.widget.Toast;
 
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -26,70 +23,53 @@ public class MainActivity extends AppCompatActivity {
     ListView activityList;
     ArrayAdapter<String> activityAdapter;
     ArrayList<String> activityList_array = new ArrayList<String>();
-    TextView chosen;
-    String hej = "ändrad";
-    String temp;
+    int posChosen;
     String stringchosen;
     final String[] RemoveEdit = {"Ändra namn", "Ta bort Aktivitet"};
     boolean editName = false;
-    //String[] newActivity = {"Fotboll", "Konferens", "Möte"};
-    FragmentManager fragmentManager = getFragmentManager();
-    CreateActivityFragment documentFrag = new CreateActivityFragment();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
-        
-        
-        
-        
+
         setContentView(R.layout.activity_main);
         activityAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, activityList_array);
         activityList = (ListView) findViewById(R.id.listView);
         activityList.setAdapter(activityAdapter);
 
         activityList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-       //Listview item listener, creates fragment on click
-        //AdapterView.OnItemClickListener itemClick = new AdapterView.OnItemClickListener() {
+
+            //Activates a onItemLongClick on all "Aktiviteter".
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                chosen = (TextView)view;
-
+                posChosen = position;
                 stringchosen = activityAdapter.getItem(position).toString();
-                Toast.makeText(MainActivity.this, stringchosen, Toast.LENGTH_SHORT).show();
                 {
                     Dialog();
                 }
-
-
                 return false;
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                FragmentTransaction transaction = fragmentManager.beginTransaction();
-//                transaction.add(R.id.main_activity_layout, documentFrag, "documentFragment");
-//                transaction.commit();
-//            }
-                //return builder.create();
             }
         });
 
     }
-
+    //Checks if the user wants to change name on an existing "Aktivitet" or delete it.
+    //If the user wants to change a name the function addOrChangeName is called.
+    //If the user wants to delete an "Aktivitet", its deleted from the list and the Dialog is closed.
     public void Dialog()
     {
-        //Toast.makeText(MainActivity.this, chosen, Toast.LENGTH_SHORT).show();
+
         AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
         builder.setTitle(stringchosen);
-        //builder.setNegativeButton("Cancel", null);
         builder.setItems(RemoveEdit, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 if (which == 0) {
                     editName= true;
+
                     addOrChangeName();
                 }
                 if (which == 1) {
-                    Toast.makeText(MainActivity.this, stringchosen, Toast.LENGTH_SHORT).show();
                     activityList_array.remove(stringchosen);
                     activityAdapter.notifyDataSetChanged();
                     dialog.dismiss();
@@ -99,6 +79,11 @@ public class MainActivity extends AppCompatActivity {
         });
         builder.show();
     }
+    //This function either adds or changes a name depending on if the boolean EditName = true/false.
+    //The boolean is set in the function Dialog above and is set to true or false depending on if the user
+    //clicked "Ändra namn" or "ta bort namn" in the RemoveEdit-string set in the Dialog.
+    //The title on the Alert will also change depending on what the user has chosen.
+    //Its not possible to enter a name which already exists.
     public void addOrChangeName()
     {
         LayoutInflater li = LayoutInflater.from(this);
@@ -109,11 +94,11 @@ public class MainActivity extends AppCompatActivity {
         final EditText userInput = (EditText) add_activityView.findViewById(R.id.user_input);
 
         builder.setCancelable(false);
-        if(editName==true)
+        if(editName)
         {
             builder.setTitle(R.string.changeName);
         }
-        if(editName==false)
+        if(!editName)
         {
             builder.setTitle(R.string.add_activity_title);
         }
@@ -122,27 +107,21 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(DialogInterface dialog, int which) {
                 if(activityList_array.contains(userInput.getText().toString()))
                 {
-
                     Toast.makeText(MainActivity.this, "Namnet måste vara unikt", Toast.LENGTH_SHORT).show();
                 }
                 else
                 {
-                    if(editName==true)
+                    if(editName)
                     {
-                        Toast.makeText(MainActivity.this, "CHECK!!!!", Toast.LENGTH_SHORT).show();
-                        temp = userInput.getText().toString();
-
-                       chosen.setText("ÄNDRAD!!!");
+                        activityList_array.set(posChosen, userInput.getText().toString());
                         activityAdapter.notifyDataSetChanged();
                     }
-                    if(editName==false)
+                    if(!editName)
                     {
                         activityList_array.add(userInput.getText().toString());
                         activityAdapter.notifyDataSetChanged();
                     }
                 }
-
-
             }
         });
         builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -154,16 +133,6 @@ public class MainActivity extends AppCompatActivity {
         AlertDialog dialog = builder.create();
         dialog.show();
     }
-
-        //Item listener
-//        AdapterView.OnItemClickListener itemClick = new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                view.setBackgroundResource(R.color.main_text_color);
-//            }
-//        };
-//        activityList.setOnItemClickListener(itemClick);
-
 
 
     @Override
@@ -181,44 +150,7 @@ public class MainActivity extends AppCompatActivity {
             case R.id.add_activity_icon:
                 editName = false;
                 addOrChangeName();
-//                LayoutInflater li = LayoutInflater.from(this);
-//                View add_activityView = li.inflate(R.layout.add_activity, null);
-//                AlertDialog.Builder builder = new AlertDialog.Builder(this);
-//                builder.setView(add_activityView);
-//
-//                final EditText userInput = (EditText) add_activityView.findViewById(R.id.user_input);
-//
-//                builder.setCancelable(false);
-//                builder.setTitle("Lägg till Aktivitet");
-//                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-//                    @Override
-//                    public void onClick(DialogInterface dialog, int which) {
-//                        if(activityList_array.contains(userInput.getText().toString()))
-//                        {
-//                            dialog.cancel();
-//                            Toast.makeText(MainActivity.this, "Namnet måste vara unikt", Toast.LENGTH_SHORT).show();
-//                        }
-//                        else
-//                        {
-//                            activityList_array.add(userInput.getText().toString());
-//                            activityAdapter.notifyDataSetChanged();
-//                        }
-//
-//
-//                    }
-//                });
-//                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-//                    @Override
-//                    public void onClick(DialogInterface dialog, int which) {
-//                        dialog.cancel();
-//                    }
-//                });
-//                AlertDialog dialog = builder.create();
-//                dialog.show();
-//
 
-
-                        Toast.makeText(MainActivity.this, "Ny aktivitet tillagd", Toast.LENGTH_SHORT).show();
             default:
                 return super.onOptionsItemSelected(item);
         }
